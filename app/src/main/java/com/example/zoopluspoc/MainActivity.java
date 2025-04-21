8a3a260c2c50355e31d4017dc4fc725dc2a5a6ce
X9010 <hamzaabdalber@gmail.com>
2025-04-22 00:40:50 +0200

    commit

--- app/src/main/java/com/example/zoopluspoc/MainActivity.java
+++ app/src/main/java/com/example/zoopluspoc/MainActivity.java
@@ -10,19 +10,15 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         TextView view = new TextView(this);
         view.setPadding(32, 64, 32, 64);
-
         Intent intent = getIntent();
         Uri data = intent.getData();
-
         if (data != null) {
             view.setText("PoC App Triggered via: " + data.toString());
         } else {
             view.setText("PoC App Launched (no link)");
         }
-
         setContentView(view);
     }
 }
